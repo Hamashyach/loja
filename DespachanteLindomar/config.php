8a61b4d7da75494c3e2995a410ead83602ebc489
Hamashyach <stephanytsm@gmail.com>
2025-11-30 22:16:24 -0300

    commit

--- DespachanteLindomar/config.php
+++
@@ -1,5 +0,0 @@
- <?php 
-// $conn = new PDO('mysql:hostname=localhost;dbname=rpvtecnologia_lindomar','rpvtecnologia_lindomarusr','rpv@senha2');
-$conn = new PDO('mysql:hostname=localhost;dbname=despachante1','root','');
-?> 
-
